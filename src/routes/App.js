6dc58ef1d2698ed670a021d2f7c0e35b7d1071c7
JRamos84 <joseph0001@gmail.com>
2023-06-22 10:24:36 -0300

    cambio el eslint

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../containers/Home';
+import Login from '../containers/Login';
+import Register from '../containers/Register';
+import NotFound from '../containers/NotFound';
+import Layout from '../components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
